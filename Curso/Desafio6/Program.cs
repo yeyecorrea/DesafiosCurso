d9cb9c95975780dfb4a205c292eb26c52c858489
Yeferson <ycorreca@summit.com.co>
2021-11-16 16:14:39 +0000

    Solucion del Desafio 6

---
+++ Curso/Desafio6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Desafio6
+{
+    class Program
+    {
+        static string nameUser = "Nanatsu";
+        static int points = 5000;
+        static void Main(string[] args)
+        {
+            PointsUser("yeferson",5000);
+        }
+
+        static void PointsUser(string user, int points)
+        {
+            if (points > Program.points)
+            {
+                Console.WriteLine($"La nueva puntuación más alta es: {points}");
+                Console.WriteLine($"La puntuación más alta fue lograda por: {user}");
+            }
+            if (points == Program.points)
+            {
+                Console.WriteLine($"Las puntuaciones son igulaes: user: {user} : {points} y user: {Program.nameUser} : {Program.points}");
+            }
+            else
+            {
+                Console.WriteLine($"La puntuación más alta es: {Program.points}");
+                Console.WriteLine($"La puntuación más alta sigue siendo lograda por: {Program.nameUser}");
+            }
+
+        }
+    }
+}
