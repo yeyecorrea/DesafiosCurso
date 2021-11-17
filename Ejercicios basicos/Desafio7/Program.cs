2428251fb4e58b34ece4d2c8c8eab9968c89618b
Yeferson <ycorreca@summit.com.co>
2021-11-17 13:23:20 +0000

    culminacion de ejercicios basicos

---
+++ Ejercicios basicos/Desafio7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Desafio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperatura;
+            string mensajeTemperatura;
+            string temperaturaIngresada;
+
+            Console.WriteLine("Ingrese la temperatura actual: ");
+            temperaturaIngresada = Console.ReadLine();
+
+            bool validationParsing = int.TryParse(temperaturaIngresada, out temperatura);
+            
+            if (validationParsing)
+            {
+                mensajeTemperatura = temperatura <= 15 ? "hace mucho frio" : (temperatura >= 16 && temperatura <= 28) ? "hace un clima agradable" : temperatura > 28 ? "hace mucho calor" : "";
+            }
+        }
+    }
+}
---
+++ Ejercicios basicos/Desafio8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Desafio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string final = "";
+            int notaEstudianta;
+            string notaIngresada;
+            int contador = 0;
+            int sumNota = 0;
+            int promedioNota;
+            int mensajeParsing;
+
+            while (final != "terminar")
+            {
+                Console.WriteLine("Ingresa nota estudiante: ");
+                notaIngresada = Console.ReadLine();
+                final = notaIngresada;
+                
+                bool verificarParsing = int.TryParse(notaIngresada, out notaEstudianta);
+                if (verificarParsing)
+                {
+                    sumNota += notaEstudianta;
+                    contador += 1;
+                }
+                else if (final == "fin")
+                {
+                    promedioNota = sumNota / contador;
+                    Console.WriteLine($"Promedio de estudiante es: {promedioNota}");
+                }
+                else{ Console.WriteLine("Tipo de dato no admitido"); }
+    
+            }
+        }
+    }
+}
